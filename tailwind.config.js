1964802a604eda3d8c5f0b18daaa2c187938ca7f
shiyammm <122890678+shiyammm@users.noreply.github.com>
2023-10-07 20:19:48 +0530

    Removed dot (.) in backgroungImage extend of tailwinfconfig file.

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,35 +16,34 @@ module.exports = {
       },
       backgroundImage: {
         'home-patternDesktop':
-          "url('./src/assets/home/background-home-desktop.jpg')",
+          "url('/src/assets/home/background-home-desktop.jpg')",
         'home-patternMobile':
-          "url('./src/assets/home/background-home-mobile.jpg')",
+          "url('/src/assets/home/background-home-mobile.jpg')",
         'home-patternTablet':
-          "url('./src/assets/home/background-home-tablet.jpg')",
+          "url('/src/assets/home/background-home-tablet.jpg')",
         'destination-patternDesktop':
-          "url('./src/assets/destination/background-destination-desktop.jpg')",
+          "url('/src/assets/destination/background-destination-desktop.jpg')",
         'destination-patternMobile':
-          "url('./src/assets/destination/background-destination-mobile.jpg')",
+          "url('/src/assets/destination/background-destination-mobile.jpg')",
         'destination-patternTablet':
-          "url('./src/assets/destination/background-destination-tablet.jpg')",
+          "url('/src/assets/destination/background-destination-tablet.jpg')",
         'crew-patternDesktop':
-          "url('./src/assets/crew/background-crew-desktop.jpg')",
+          "url('/src/assets/crew/background-crew-desktop.jpg')",
         'crew-patternMobile':
-          "url('./src/assets/crew/background-crew-mobile.jpg')",
+          "url('/src/assets/crew/background-crew-mobile.jpg')",
         'crew-patternTablet':
-          "url('./src/assets/crew/background-crew-tablet.jpg')",
+          "url('/src/assets/crew/background-crew-tablet.jpg')",
         'technology-patternDesktop':
-          "url('./src/assets/technology/background-technology-desktop.jpg')",
+          "url('/src/assets/technology/background-technology-desktop.jpg')",
         'technology-patternMobile':
-          "url('./src/assets/technology/background-technology-mobile.jpg')",
+          "url('/src/assets/technology/background-technology-mobile.jpg')",
         'technology-patternTablet':
-          "url('./src/assets/technology/background-technology-tablet.jpg')",
+          "url('/src/assets/technology/background-technology-tablet.jpg')",
       },
       screens: {
         '2/5xl': '1870px',
       },
-      center: {
-      }
+      center: {},
     },
   },
   plugins: [],
